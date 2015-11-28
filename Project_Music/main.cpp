69b9716f48cc31cff96cf15653d898174ae3be6e
PlatPlat48 <roberts.chance@gmail.com>
2015-11-27 18:43:19 -0700

    Initial Commit

---
+++ Project_Music/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project_Music
+//
+//  Created by Chance Roberts on 11/27/15.
+//  Copyright © 2015 Chance Roberts. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
